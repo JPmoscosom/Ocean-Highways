822789e551808aa3156b501ba9389fd54b4a453a
Juan Pablo Moscoso Mesa <36666466+JPmoscosom@users.noreply.github.com>
2021-10-01 06:52:32 -0500

    Add files via upload

---
+++ pages/home/administrador.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <v-row>
+      <v-col>
+        <v-btn color="success" to='/usuarios'>Gestionar Usuario</v-btn>
+      </v-col>
+      <v-col>
+        <v-btn color="success" to="/reservas">Gestionar Reservas</v-btn>
+      </v-col>
+      <v-col>
+        <v-btn color="success" to="/destinos">Gestionar Destinos</v-btn>
+      </v-col>
+    </v-row>
+  </div>
+</template>
